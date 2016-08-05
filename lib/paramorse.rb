6a7feb24e6a85947f00a2e0724b7aa55f7a62747
Ryan Workman <rdavid1099@gmail.com>
2016-08-05 00:11:23 -0600

    Added decoder capability to decode complex sentences with spec chars

--- lib/paramorse.rb
+++ lib/paramorse.rb
@@ -10,14 +10,33 @@ module ParaMorse
     end
 
     def decode(morse)
+      morse_words = split_morse_words(morse)
+      morse_words.map do |word|
+        decode_morse_words(word)
+      end.join
+    end
+
+    def decode_morse_words(morse)
       morse_letters = split_morse_letters(morse)
       morse_letters.map do |morse_letter|
         letter_decoder.decode(morse_letter)
       end.join
     end
 
+    def split_morse_words(words)
+      return [words] unless words.include?("0000000")
+      words.split("0000000").reduce([]) do |result, word|
+        result << word
+        result << "0000000"
+      end
+    end
+
     def split_morse_letters(morse)
-      morse.split("000")
+      unless morse == "0000000"
+        morse.split("000")
+      else
+        [morse]
+      end
     end
 
   end
