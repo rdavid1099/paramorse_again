1bc77e22a4bbac31362df62f7df9e2f487d89efb
Ryan Workman <rdavid1099@gmail.com>
2016-08-04 20:38:40 -0600

    Implemented basic functionality for queue

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,1 +1,54 @@
+module ParaMorse
 
+  class Queue
+    attr_reader :queue
+
+    def initialize
+      @queue = Array.new
+    end
+
+    def push(digit)
+      queue << digit
+    end
+
+    def pop(amount = 1)
+      if amount == 1
+        queue.shift
+      else
+        pop_multiple(amount)
+      end
+    end
+
+    def pop_multiple(amount)
+      popped_elements = Array.new
+      amount.times do
+        popped_elements << queue.shift
+      end
+      popped_elements
+    end
+
+    def peek(amount = 1)
+      if amount == 1
+        queue[0]
+      else
+        queue[0..amount - 1]
+      end
+    end
+
+    def tail(amount = 1)
+      if amount == 1
+        queue[-1]
+      else
+        queue.reverse[0..amount - 1]
+      end
+    end
+
+    def count
+      queue.length
+    end
+
+  end
+
+
+
+end
