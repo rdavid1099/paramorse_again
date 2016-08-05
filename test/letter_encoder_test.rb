d9df5a3d0ae5021869c73e09a8876f69b5d4a501
Ryan Workman <rdavid1099@gmail.com>
2016-08-04 22:10:09 -0600

    Letter decoder and encoder fully implemented and handling special characters

--- test/letter_encoder_test.rb
+++ test/letter_encoder_test.rb
@@ -1,35 +1,35 @@
 require "./lib/paramorse"
 require "./test/test_helper"
 
-class TestEncoder < Minitest::Test
+class TestLetterEncoder < Minitest::Test
   def test_encoder_exists
-    letter_encoder = ParaMorse::Encoder.new
+    letter_encoder = ParaMorse::LetterEncoder.new
 
-    assert_instance_of ParaMorse::Encoder, letter_encoder
+    assert_instance_of ParaMorse::LetterEncoder, letter_encoder
   end
 
   def test_encoder_can_receive_a_letter_to_encode
-    letter_encoder = ParaMorse::Encoder.new
+    letter_encoder = ParaMorse::LetterEncoder.new
 
     assert_equal "10111", letter_encoder.encode("a")
   end
 
   def test_encoder_can_receive_a_different_letter_to_encode
-    letter_encoder = ParaMorse::Encoder.new
+    letter_encoder = ParaMorse::LetterEncoder.new
 
     assert_equal "10111", letter_encoder.encode("a")
     assert_equal "1110111010111", letter_encoder.encode("q")
   end
 
   def test_encoder_is_case_insensitive
-    letter_encoder = ParaMorse::Encoder.new
+    letter_encoder = ParaMorse::LetterEncoder.new
 
     assert_equal "111010101", letter_encoder.encode("B")
     assert_equal "1011101", letter_encoder.encode("R")
   end
 
   def test_encoder_can_handle_special_characters
-    letter_encoder = ParaMorse::Encoder.new
+    letter_encoder = ParaMorse::LetterEncoder.new
 
     assert_equal "10111010111010111", letter_encoder.encode(".")
     assert_equal "1110111010101110111", letter_encoder.encode(",")
@@ -37,7 +37,7 @@ class TestEncoder < Minitest::Test
   end
 
   def test_encoder_evaluates_valid_and_invalid_letters
-    letter_encoder = ParaMorse::Encoder.new
+    letter_encoder = ParaMorse::LetterEncoder.new
 
     assert_equal true, letter_encoder.valid_letter?("a")
     assert_equal true, letter_encoder.valid_letter?("R")
@@ -46,7 +46,7 @@ class TestEncoder < Minitest::Test
   end
 
   def test_encoder_returns_character_if_it_is_not_understood
-    letter_encoder = ParaMorse::Encoder.new
+    letter_encoder = ParaMorse::LetterEncoder.new
 
     assert_equal "<", letter_encoder.encode("<")
     assert_equal ">", letter_encoder.encode(">")
