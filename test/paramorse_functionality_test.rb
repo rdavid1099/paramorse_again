b94ca08dbaa9462a67274c28ad8055ce12c5ed8e
Ryan Workman <rdavid1099@gmail.com>
2016-08-06 13:29:08 -0600

    Added edge case testing and ParaMorse functioning with 5000+ lines of Hamlet

--- test/paramorse_functionality_test.rb
+++ test/paramorse_functionality_test.rb
@@ -18,4 +18,31 @@ class TestParaMorseFunctionality < Minitest::Test
 
     assert_equal expected, actual
   end
+
+  def test_parallel_encoder_and_decoder_can_handle_HAMLET
+    parallel_encoder = ParaMorse::ParallelEncoder.new
+    parallel_decoder = ParaMorse::ParallelDecoder.new
+    parallel_encoder.encode_from_file('hamlet.txt', 8, 'hamlet_output*.txt')
+
+    8.times do |number|
+      assert_equal true, File.exist?("./encoded_files/hamlet_output#{number}.txt")
+    end
+
+    parallel_decoder.decode_from_files(8, 'hamlet_output*.txt', 'hamlet_decoded.txt')
+    expected = File.read("./original_files/hamlet.txt").downcase
+    actual = File.read("./decoded_files/hamlet_decoded.txt")
+
+    assert_equal expected, actual
+  end
+
+  def test_edge_case_of_more_encoders_and_decoders_than_characters_in_file
+    parallel_encoder = ParaMorse::ParallelEncoder.new
+    parallel_decoder = ParaMorse::ParallelDecoder.new
+    parallel_encoder.encode_from_file('small_file.txt', 15, 'small_output*.txt')
+    parallel_decoder.decode_from_files(15, 'small_output*.txt', 'small_decoded.txt')
+    expected = File.read("./original_files/small_file.txt").downcase
+    actual = File.read("./decoded_files/small_decoded.txt")
+
+    assert_equal expected, actual
+  end
 end
